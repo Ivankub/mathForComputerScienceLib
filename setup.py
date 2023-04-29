1e1f72bd22469f69c6185dfa6a2c58be291dc9dc
Ivankub <vanadolgun@gmail.com>
2023-04-29 22:45:47 +0700

    real 0.3.2

--- setup.py
+++ setup.py
@@ -1,11 +1,13 @@
-from setuptools import find_packages, setup
+from setuptools import setup
+import codecs
 
 setup(
     name='MKN_third_codes',
-    version='0.3.1',
+    version='0.3.2',
     description='Python library with standart solutions for probability tasks',
     author='Dolgun Ivan',
     author_email='vanadolgun@gmail.com',
     packages=['MKN_third_codes/'],
-    license='MIT'
+    license='MIT',
+    long_description=codecs.open("./README.md", "r", "utf_8_sig").read()
 )
