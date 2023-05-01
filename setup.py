c8b85062a8d38f44753754f10357f4bdba292742
Ivankub <vanadolgun@gmail.com>
2023-05-01 11:53:44 +0700

    real 0.4.4

--- setup.py
+++ setup.py
@@ -9,6 +9,6 @@ setup(
     author_email='vanadolgun@gmail.com',
     packages=['MKN_third_codes/'],
     license='MIT',
-    long_description= open('./README.md', 'r', encoding="utf-8").read() + '\n\n' + open('./CHANGELOG.md', 'r', encoding="utf-8").read(),
+    long_description= open('./README.md', 'r', encoding="utf-8").read() + '\n#\n' + open('./CHANGELOG.md', 'r', encoding="utf-8").read(),
     long_description_content_type='text/markdown'
 )
